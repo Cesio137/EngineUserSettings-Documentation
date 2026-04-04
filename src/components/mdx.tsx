cdc6570b46c417e1c2153f92ba713765d3affa5c
Nathan Miguel <nathan.cm77@gmail.com>
2026-04-04 19:19:36 -0300

    downgrade react image zoom

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -1,12 +1,10 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 import { UVar, UEvents, UFunction } from '@components';
-import { ImageZoom } from 'fumadocs-ui/components/image-zoom';
 
 export function getMDXComponents(components?: MDXComponents) {
   return {
     ...defaultMdxComponents,
-    img: (props) => <ImageZoom {...(props as any)} />,
     ...components,
     UVar,
     UEvents,
